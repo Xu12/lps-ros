1317c2b808874271cae3dd1cf3cf7bd0e3728b80
rainlongchen <rainlongchen@gmail.com>
2017-05-04 10:26:38 +0800

    Add msg format in uwbtag_link node

--- src/uwbtag_link.cpp
+++ src/uwbtag_link.cpp
@@ -15,17 +15,16 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Twist.h>
-//#include <cereal_port/CerealPort.h>
 #include <sensor_msgs/Imu.h>
-//#include <uwbtag_link/UWBNode.h>
+#include <bitcraze_lps_estimator/UwbRange.h>
 using namespace std;
 
 //ROS Parameter
-#define MAIN_FREQ           5000.0       //Controller main frequency
+#define MAIN_FREQ           5000.0      //Controller main frequency
 #define TF_REV_DELAY_T      150         //TF of MAP_ODOM frame receive start delay units * ts
-#define USE_REV_ENCODE      1
 #define DEBUG_FLAG          1
 #define ANCHOR_NUM          6
+#define TAG_ID              8
 
 #define REPLY_SIZE 1
 #define TIMEOUT 10
@@ -36,25 +35,15 @@ using namespace std;
 #define PI           3.14159
 #define RTD          180.0/PI           //Radian to Degree
 #define DTR          PI/180.0           //Degree to Radian
-#define TS 0.1                          //unit:s
 
-#define AXON_ROBOT_R 0.1075             //unit:m
-#define AXON_ROBOT_L 0.2680             //unit:m
-#define AXON_WHEEL_MINSPEED 0.0065      //(rad/s)
-#define AXON_WHEEL_MAXSPEED 7.5         //(rad/s)
-#define AXON_WHEEL_CMD_LIMIT 45         //AXON Command 0~127, 45:wheel velocity limit in 3.0(rad/s)
-
-#define ONE_R_ENCODER_PLUES     25600.0                    //unit:pluse
-#define RAD_PLUSE               PI/ONE_R_ENCODER_PLUES     //unit:rad/pluse
-
-#define GETSTR_PORT "/uwbtag_parameter/SerialPort"
-#define GETSTR_RATE "/uwbtag_parameter/BaudRate"
+#define GETSTR_PORT "/UWBTag_SerialPort"
+#define GETSTR_RATE "/UWBTag_BaudRate"
 
 //------------------------------------------
 double main_freq_=MAIN_FREQ;
 double tf_rev_delay_t_=TF_REV_DELAY_T;
-int use_rev_encode_=USE_REV_ENCODE;
 int debug_flag_=DEBUG_FLAG;
+int tag_id=TAG_ID;
 
 typedef struct
 {
@@ -64,24 +53,16 @@ typedef struct
 } Pose;
 
 //----------------------------------------------------------
-ros::Subscriber axon_info_sub_;
-//Publish the AXON info. over ROS
-ros::Time current_time, last_time, odom_current_time, odom_last_time;
+//Publish the UWB Range info. over ROS
+ros::Time current_time, last_time;
 
-int baud_rate=9600;
 std::string serial_port;
-//cereal::CerealPort device;
+int baud_rate=9600;
 char reply[REPLY_SIZE];
-int cmd_type=0;
 unsigned char rx_buff[RX_LEN];
 int rx_count=0;
 char rx_timeout_flag=0;
 int rx_timeout_cnt=0;
-
-double total_time=0;
-int rx_stop_flag=0;
-int spin_cnt=0;
-int tx_cmd_send_cnt=0;
 
 //Quaternion to All Euler angle
 Pose Quat_to_AllEuler(geometry_msgs::Quaternion msg_quat)
@@ -154,10 +135,11 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "uwbtag_link_node");
     ros::NodeHandle n;
 
+    ros::Publisher uwb_pub = n.advertise<bitcraze_lps_estimator::UwbRange>("uwbrange", 100);
+
+    bitcraze_lps_estimator::UwbRange uwb_msg;
     current_time = ros::Time::now();
     last_time = ros::Time::now();
-    odom_current_time = ros::Time::now();
-    odom_last_time = ros::Time::now();
 
     int rx_count=0;
     char rx_flag=0;
@@ -166,27 +148,39 @@ int main(int argc, char** argv)
     unsigned char rx_buff_num[6];
     char rx_sign=0;
     double rx_num=0;
-    double rx_exp=1;
+    double rx_exp=0.001;
 
     int fd;
-    int wr, rd;
+    int rd;
+    //int wr;
     char buff;
     struct termios options;
+
+    serial_port="/dev/ttyACM0";
+    baud_rate=9600;
+
+    double distance_err[ANCHOR_NUM];
+    std::vector<double> anchor_pos[ANCHOR_NUM];
 
     //==================================================
     //load ROS Parameter
     n.getParam("main_freg", main_freq_);
     n.getParam("tf_rev_delay_t", tf_rev_delay_t_);
-    n.getParam("use_rev_encode", use_rev_encode_);
     n.getParam("debug_flag", debug_flag_);
     n.getParam(GETSTR_PORT, serial_port);
-    ROS_INFO("I get SerialPort: %s", serial_port.c_str());
     n.getParam(GETSTR_RATE, baud_rate);
-    ROS_INFO("I get BaudRate: %d", baud_rate);
-
-    serial_port="/dev/ttyACM0";
-    baud_rate=9600;
-
+    n.getParam("anchor0_pos", anchor_pos[0]);
+    n.getParam("anchor1_pos", anchor_pos[1]);
+    n.getParam("anchor2_pos", anchor_pos[2]);
+    n.getParam("anchor3_pos", anchor_pos[3]);
+    n.getParam("anchor4_pos", anchor_pos[4]);
+    n.getParam("anchor5_pos", anchor_pos[5]);
+    n.getParam("distance0_err", distance_err[0]);
+    n.getParam("distance1_err", distance_err[1]);
+    n.getParam("distance2_err", distance_err[2]);
+    n.getParam("distance3_err", distance_err[3]);
+    n.getParam("distance4_err", distance_err[4]);
+    n.getParam("distance5_err", distance_err[5]);
     /*
     try{ device.open(serial_port.c_str(), baud_rate); }
     catch(cereal::Exception& e)
@@ -197,33 +191,26 @@ int main(int argc, char** argv)
     ROS_INFO("The serial port is opened.");
     */
 
-    rx_stop_flag=0;
-
-    /* open the port */
-    fd=open(serial_port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    fd=open(serial_port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY); /* open the port */
     if (fd == -1 )
     {
         perror("open_port: Unable to open serial port\n");
-        ROS_FATAL("Failed to open the serial port!!!");
+        ROS_FATAL("UWB: Failed to open the serial port!!!");
         ROS_BREAK();
     }
     else
     {
         fcntl(fd, F_SETFL,0);
-        /* get the current options */
-        tcgetattr(fd, &options);
-
+        tcgetattr(fd, &options); /* get the current options */
         /* set raw input, 1 second timeout */
         options.c_cflag     |= (CLOCAL | CREAD);
         options.c_lflag     &= ~(ICANON | ECHO | ECHOE | ISIG);
         options.c_oflag     &= ~OPOST;
         options.c_cc[VMIN]  = 0;
         options.c_cc[VTIME] = 10;
-
         /* set the baud rates to 9600... */
         cfsetispeed(&options, B9600);
         cfsetospeed(&options, B9600);
-
         /* set the options */
         tcsetattr(fd, TCSANOW, &options);
         printf("Port 1 has been sucessfully opened and %d is the file description\n", fd);
@@ -290,10 +277,10 @@ int main(int argc, char** argv)
                             rx_buff_num[i]=0;
                     }
 
-                    printf("1=%d, 2=%d, 3=%d, 4=%d, 5=%d, 6=%d\n", rx_buff_num[0], rx_buff_num[1], rx_buff_num[2] ,rx_buff_num[3], rx_buff_num[4], rx_buff_num[5]);
+                    //printf("1=%d, 2=%d, 3=%d, 4=%d, 5=%d, 6=%d\n", rx_buff_num[0], rx_buff_num[1], rx_buff_num[2] ,rx_buff_num[3], rx_buff_num[4], rx_buff_num[5]);
 
                     rx_num=0.0;
-                    rx_exp=1.0;
+                    rx_exp=0.001;
                     for (int i=0;i<6;i++)
                     {
                         rx_num=rx_num+(double)rx_buff_num[5-i]*rx_exp;
@@ -305,7 +292,20 @@ int main(int argc, char** argv)
                             ranges[rx_id-1]=-rx_num;
                         else
                             ranges[rx_id-1]=rx_num;
+
                         printf("ID%d=%f\n", rx_id, ranges[rx_id-1]);
+                        current_time = ros::Time::now();
+                        uwb_msg.header.stamp = current_time;
+                        uwb_msg.header.frame_id = "range";
+                        uwb_msg.requester_id=tag_id;
+                        uwb_msg.responder_id=rx_id-1;
+                        uwb_msg.distance=ranges[rx_id-1];
+                        uwb_msg.distance_err=distance_err[rx_id-1];
+                        uwb_msg.antenna=0;
+                        uwb_msg.responder_location.x=anchor_pos[rx_id-1][0];
+                        uwb_msg.responder_location.y=anchor_pos[rx_id-1][1];
+                        uwb_msg.responder_location.z=anchor_pos[rx_id-1][2];
+                        uwb_pub.publish(uwb_msg);
                     }
                 }//end if ( (rx_buff[0]=='s') && ...
                 rx_flag=0;
